010cfc93bef8470a52f683a1c81fc807df7f8ee5
makopy <makopy.inside@gmail.com>
2015-04-15 12:02:28 +0900

    Modify 'UIImage+Ex'

--- Category/UIImage+Ex/UIImage+Ex.h
+++ Category/UIImage+Ex/UIImage+Ex.h
@@ -6,8 +6,13 @@
 
 @interface UIImage (Ex)
 
+#pragma mark - Generate UIImage
 + (UIImage *)imageFromView:(UIView *)view;
 + (UIImage *)imageFromWindow:(UIWindow *)window;
 + (UIImage *)imageFromKeyWindow;
+#pragma mark - Generate CGImageRef
++ (CGImageRef)CGImageFromView:(UIView *)view;
++ (CGImageRef)CGImageFromWindow:(UIWindow *)window;
++ (CGImageRef)CGImageFromKeyWindow;
 
 @end
--- Category/UIImage+Ex/UIImage+Ex.m
+++ Category/UIImage+Ex/UIImage+Ex.m
@@ -6,12 +6,17 @@
 
 @implementation UIImage (Ex)
 
+#pragma mark - Generate UIImage
+
 + (UIImage *)imageFromView:(UIView *)view {
     
-    UIGraphicsBeginImageContextWithOptions(view.bounds.size, NO, 0.0f);
+    UIImage *image;
+    
+    //UIGraphicsBeginImageContext(view.frame.size);
+    UIGraphicsBeginImageContextWithOptions(view.frame.size, NO, 0.0f);
     CGContextRef context = UIGraphicsGetCurrentContext();
     [view.layer renderInContext:context];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
     return image;
@@ -19,9 +24,7 @@
 
 + (UIImage *)imageFromWindow:(UIWindow *)window {
     
-    if (window == nil) {
-        return nil;
-    }
+    UIImage *image;
     
     UIGraphicsBeginImageContextWithOptions(window.bounds.size, NO, 0.0f);
     CGContextRef context = UIGraphicsGetCurrentContext();
@@ -30,7 +33,7 @@
         [aWindow.layer renderInContext:context];
     }
     
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
     return image;
@@ -41,6 +44,23 @@
     UIWindow *window = [[UIApplication sharedApplication] keyWindow];
     
     return [self imageFromWindow:window];
+}
+
+#pragma mark - Generate CGImageRef
+
++ (CGImageRef)CGImageFromView:(UIView *)view {
+    
+    return [[self imageFromView:view] CGImage];
+}
+
++ (CGImageRef)CGImageFromWindow:(UIWindow *)window {
+    
+    return [[self imageFromWindow:window] CGImage];
+}
+
++ (CGImageRef)CGImageFromKeyWindow {
+    
+    return [[self imageFromKeyWindow] CGImage];
 }
 
 @end
